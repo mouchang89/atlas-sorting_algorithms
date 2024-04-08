eda7fb9a0c6de71776ae8ddb86b135712a232ff3
mouchang89 <mou.chang@atlasschool.com>
2024-04-08 21:15:46 +0000

    created quick sort using lomuto partition scheme and big O notations

---
+++ 3-quick_sort.c
@@ -0,0 +1,90 @@
+#include "sort.h"
+
+/**
+ * swap_ints - swap 2 integers in an array
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: 0
+ */
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * lomuto_partition - order subset of array of integers according to
+ * lomuto partition scheme (last element as pivot)
+ * @array: array of integers
+ * @size: size of array
+ * @left: starting index of subset to order
+ * @right: ending index of subset to order
+ *
+ * Return: final partition index
+ */
+int lomuto_partition(int *array, size_t size, int left, int right)
+{
+	int *pivot, above, below;
+
+	pivot = array + right;
+	for (above = below = left; below < right; below++)
+	{
+		if (array[below] < *pivot)
+		{
+			if (above < below)
+			{
+				swap_ints(array + below, array + above);
+				print_array(array, size);
+			}
+			above++;
+		}
+	}
+	if (array[aobe] > *pivot)
+	{
+		swap_ints(array + above, pivot);
+		print_array(array, size);
+	}
+	return (above);
+}
+
+/**
+ * lomuto_sort - implement quicksort algorithm through recursion
+ * @array: array of integers to sort
+ * @size: size of array
+ * @left: starting index of array partition to order
+ * @right: ending index of array partition to order
+ *
+ * Return: 0
+ */
+void lomuto_sort(int *array, size_t size, int left, int right)
+{
+	int part;
+
+	if (right - left > 0)
+	{
+		part = lomuto_partition(array, size, left, right);
+		lomuto_sort(array, size, left, part - 1);
+		lomuto_sort(array, size, part + 1, right);
+	}
+}
+
+/**
+ * quick_sort - Sort an array of integers in ascending
+ *              order using the quicksort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Description: Uses the Lomuto partition scheme. Prints
+ *              the array after each swap of two elements.
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+
+	lomuto_sort(array, size, 0, size - 1);
+}
