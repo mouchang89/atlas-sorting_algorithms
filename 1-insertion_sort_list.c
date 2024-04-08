f783abde3fd94f4bf6f632d8efb4129a45471314
mouchang89 <mou.chang@atlasschool.com>
2024-04-08 20:33:09 +0000

    added () to *n1 and fixed spelling for list

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -10,22 +10,22 @@
  */
 void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
 {
-	*n1->next = n2->next;
+	(*n1)->next = n2->next;
 	if (n2->next != NULL)
 	{
 		n2->next->prev = *n1;
 	}
-	n2->prev = *n1->prev;
+	n2->prev = (*n1)->prev;
 	n2->next = *n1;
-	if (*n1->prev != NULL)
+	if ((*n1)->prev != NULL)
 	{
-		*n1->prev->next = n2;
+		(*n1)->prev->next = n2;
 	}
 	else
 	{
 		*h = n2;
 	}
-	*n1->prev = n2;
+	(*n1)->prev = n2;
 	*n1 = n2->prev;
 }
 
@@ -40,11 +40,11 @@ void insertion_sor_list(listint_t **list)
 {
 	listint_t *iter, *insert, *tmp;
 
-	if (lsit == NULL || *lsit == NULL || *list-> == NULL)
+	if (list == NULL || *lsit == NULL || *list-> == NULL)
 	{
 		return;
 	}
-	for (iter = *lsit->next; iter != NULL; iter = tmp)
+	for (iter = *list->next; iter != NULL; iter = tmp)
 	{
 		tmp = iter->next;
 		insert = iter-> prev;
